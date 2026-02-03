d1f029e57bc068fa68e93a5b881a80a22b368773
Mark Kinyanjui <gitaumark502@gmail.com>
2026-02-03 16:32:59 +0300

    comment out the prisma usage

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-});
+// const prisma = new PrismaClient({
+//   log:
+//     process.env.NODE_ENV === "development"
+//       ? ["query", "error", "warn"]
+//       : ["error"],
+// });
 
-// Handle graceful shutdown
-process.on("beforeExit", async () => {
-  await prisma.$disconnect();
-});
+// // Handle graceful shutdown
+// process.on("beforeExit", async () => {
+//   await prisma.$disconnect();
+// });
 
-export default prisma;
+// export default prisma;
